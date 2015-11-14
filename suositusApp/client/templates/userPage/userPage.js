bcd05fc32fe8b553174ae6d45fdd589e1d78aca0
Saturator <matias.enkovaara@gmail.com>
2015-11-14 21:07:56 +0200

    Update userPage.js

--- suositusApp/client/templates/userPage/userPage.js
+++ suositusApp/client/templates/userPage/userPage.js
@@ -5,7 +5,7 @@ Template.add.events({
         //haetaan targetti joka on inputin nimeltä 'like' arvo
         var like = event.target.like.value;
 
-        //lisätään jokaisen välin tilalle plussa jotta sen voi laittaa urliin
+        //lisataan jokaisen välin tilalle plussa jotta sen voi laittaa urliin
         like = like.replace(' ', '+'); //james+bond
         //OMDB:n urli josta haetaan kaikki leffan IMDB tiedot
         var likeUrl = 'http://www.omdbapi.com/?t=' + like + '&y=&plot=short&r=json';
@@ -20,17 +20,17 @@ Template.add.events({
             }
         });
 
-        //tänne pitää lisätä vielä systeemi ettei voi lisätä samaa leffa kahta kertaa
+        //tanne pitaa lisata viela systeemi ettei voi lisata samaa leffa kahta kertaa
 
         //laitetaan väli takas leffan nimeen
         like = like.replace('+', ' '); //james bond
 
-        //viitataan saveLike metodiin ja lähetetään sinne like muuttujan sisältämät arvot
+        //viitataan saveLike metodiin ja lahetetaan sinne like muuttujan sisaltamat arvot
         Meteor.call('saveLike', like);
 
         console.log(likeUrl);
 
-        //buutataan text inputti takas tekstittömäksi
+        //buutataan text inputti takas tekstittomaksi
         event.target.like.value = "";
     }
 });
