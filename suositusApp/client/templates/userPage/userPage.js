b29d9920b55929d84fb503ed8476587e5cb759b1
saturator <matias.enkovaara@gmail.com>
2015-11-14 20:27:38 +0200

    GetOMDB

--- suositusApp/client/templates/userPage/userPage.js
+++ suositusApp/client/templates/userPage/userPage.js
@@ -3,8 +3,21 @@ Template.add.events({
         event.preventDefault();
 
         var like = event.target.like.value;
-        Likes.insert({title: like});
+        like = like.replace(' ', '+'); //james+bond
+        var likeUrl = 'http://www.omdbapi.com/?t=' + like + '&y=&plot=short&r=json';
 
+        HTTP.get(likeUrl, function(error, response) {
+            if(error) {
+                console.log(error);
+            }
+            else {
+                console.log(response.content);
+            }
+        });
+
+        like = like.replace('+', ' '); //james+bond
+        Likes.insert({title: like});
+        console.log(likeUrl);
         event.target.like.value = "";
     }
 });
